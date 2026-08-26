75b4149122f9a83f60880af9971bdedd01fc70df
Berlyng <berlyngyg@gmail.com>
2026-08-26 13:31:04 -0400

    feat: add user domain entity

---
+++ src/Distribuidora.Domain/Users/UserErrors.cs
@@ -0,0 +1,15 @@
+﻿using Distribuidora.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace Distribuidora.Domain.Users
+{
+    public sealed class UserErrors
+    {
+        public static readonly Error Blocked = new("User.Blocked", "El usuario esta bloqueado");
+
+        public static readonly Error Suspended = new("User.Suspended", "El usuario esta suspendido");
+    }
+}
