974f788190b8e5134c041c900d30df6d6dffb1b2
Berlyng <berlyngyg@gmail.com>
2026-08-26 12:49:03 -0400

    feat: add passworhash value object

---
+++ src/Distribuidora.Domain/Users/ValueObject/PasswordHash.cs
@@ -0,0 +1,35 @@
+﻿using Distribuidora.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Distribuidora.Domain.Users.ValueObject
+{
+    public sealed record PasswordHash
+    {
+        public PasswordHash(string value)
+        {
+            Value = value;
+        }
+
+        public string Value { get; }
+
+       public static Result<PasswordHash> Create(string value)
+        {
+            if (string.IsNullOrEmpty(value))
+            {
+                return Result<PasswordHash>.Failure(new Error("PasswordHash.Required", "El hash no puede estar vacio"));
+
+            }
+
+            return Result<PasswordHash>.Success(new PasswordHash(value));
+        }
+
+        public static PasswordHash FromPersistence(string value)
+        {
+            return new PasswordHash(value);
+        }
+
+        public override string ToString() => "********";
+    }
+}
