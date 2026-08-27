a314e1e48e4959fade56924826ff919da55a6aba
Berlyng <berlyngyg@gmail.com>
2026-08-27 11:02:15 -0400

    feat: implement passwordhasher

---
+++ src/Distribuidora.Infrastructure/Security/BCryptPasswordHasher.cs
@@ -0,0 +1,22 @@
+﻿using Distribuidora.Application.Users.Abstractions;
+using Distribuidora.Domain.Users.ValueObject;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Distribuidora.Infrastructure.Security
+{
+    public sealed class BCryptPasswordHasher : IPasswordHasher
+    {
+        public PasswordHash Hash(Password password)
+        {
+            var hash = BCrypt.Net.BCrypt.HashPassword(password.Value);
+            return PasswordHash.Create(hash).Value;
+        }
+
+        public bool Verify(Password password, PasswordHash hash)
+        {
+            return BCrypt.Net.BCrypt.Verify(password.Value, hash.Value);
+        }
+    }
+}
