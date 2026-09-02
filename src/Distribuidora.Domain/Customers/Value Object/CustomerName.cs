517608693ff5dcc786dd5efb15b4d40454aa2d0a
Berlyng <berlyngyg@gmail.com>
2026-09-02 11:08:07 -0400

    feat: add customer value objects

---
+++ src/Distribuidora.Domain/Customers/Value Object/CustomerName.cs
@@ -0,0 +1,37 @@
+﻿using Distribuidora.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Distribuidora.Domain.Customers.Value_Object
+{
+    public sealed record CustomerName
+    {
+        public CustomerName(string value)
+        {
+            Value = value;
+        }
+
+        public string Value { get; }
+
+
+        public static Result<CustomerName> Create(string value)
+        {
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                return Result<CustomerName>.Failure(new Error("Customer.Required", "El nombre del cliente es obligatorio"));
+            }
+
+            value = value.Trim();
+
+            if (value.Length > 150)
+            {
+                return Result<CustomerName>.Failure(new Error ("Customer.NameTooLong", "El nombre del cliente no puede exceder los 150 caracteres"));
+            }
+            return Result<CustomerName>.Success(new CustomerName(value));
+
+        }
+
+        public override string ToString() => Value;
+    }
+}
---
+++ src/Distribuidora.Domain/Customers/Value Object/PhoneNumber.cs
@@ -0,0 +1,48 @@
+﻿using Distribuidora.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Distribuidora.Domain.Customers.Value_Object
+{
+    public sealed record PhoneNumber
+    {
+        public PhoneNumber(string value)
+        {
+            Value = value;
+        }
+
+        public string Value { get; }
+
+        public static Result<PhoneNumber> Create(string value)
+        {
+            if (string.IsNullOrEmpty(value))
+            {
+                return Result<PhoneNumber>.Failure(new Error("Customer.PhoneRequired" ,"El numero de telefono es requerido"));
+            }
+
+            var normalized = value
+                .Replace("-", string.Empty)
+                .Replace("(", string.Empty)
+                .Replace(")", string.Empty)
+                .Replace(" ", string.Empty)
+                .Trim();
+
+            if (!normalized.All(char.IsDigit))
+            {
+                return Result<PhoneNumber>.Failure(new Error("Customer.InvalidPhone", "El numero de telefono solo puede contener numeroes"));
+            }
+
+            if(normalized.Length != 10)
+            {
+                return Result<PhoneNumber>.Failure(new Error("Customer.InvalidPhone", "El numero de telefono debe tener 10 digitos"));
+            }
+
+            return Result<PhoneNumber>.Success(new PhoneNumber(normalized));
+        }
+
+
+        public override string ToString() => Value;
+
+    }
+}
