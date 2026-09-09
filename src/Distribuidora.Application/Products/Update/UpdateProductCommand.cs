bc153dfb6f11aa4bd051ead06ba7ac0a9f7e5bcd
Berlyng <berlyngyg@gmail.com>
2026-09-09 18:38:02 -0400

    feat: add update product

---
+++ src/Distribuidora.Application/Products/Update/UpdateProductCommand.cs
@@ -0,0 +1,16 @@
+﻿using Distribuidora.Domain.Common;
+using Distribuidora.Domain.Products;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Distribuidora.Application.Products.Update
+{
+    public sealed record UpdateProductCommand(
+        Guid ProductId,
+        string Name,
+        string? Description,
+        ProductTaxType TaxType) : IRequest<Result>;
+   
+}
