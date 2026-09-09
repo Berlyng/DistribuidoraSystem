b5321fbd0f90f7913116e90d331b550e213bcdab
Berlyng <berlyngyg@gmail.com>
2026-09-09 15:13:39 -0400

    feat: add get product by id

---
+++ src/Distribuidora.Application/Products/GetById/GetProductByIdQuery.cs
@@ -0,0 +1,9 @@
+﻿using Distribuidora.Domain.Common;
+using MediatR;
+
+namespace Distribuidora.API.Products.GetById
+{
+    public sealed record GetProductByIdQuery(
+        Guid Id) : IRequest<Result<ProductResponse>>;
+ 
+}
